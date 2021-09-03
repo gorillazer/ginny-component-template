8f92578072f0e6fae95c86bdf40da6bfd57d7ab0
linyyyang <linyyyang@tencent.com>
2021-09-03 17:07:13 +0800

    🐞 fix:handle

---
+++ handlers/handler.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/google/wire"
+	"github.com/gorillazer/ginny-serve/http"
+	// "github.com/gorillazer/ginny/res"
+)
+
+func CreateInitHandlerFn(
+// HANDLE 命令行工具锚点,请勿删除本行注释! Important! Do not delete this line
+) http.InitHandlers {
+	return func(r *gin.Engine) {
+		// r.GET("/test/:id", res.Wrapper(test.Get))
+	}
+}
+
+var ProviderSet = wire.NewSet(
+	// HANDLE_PROVIDER 命令行工具锚点,请勿删除本行注释! Important! Do not delete this line
+	CreateInitHandlerFn,
+)
